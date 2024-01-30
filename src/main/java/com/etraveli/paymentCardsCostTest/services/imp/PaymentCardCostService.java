1ee51d1874ea70c24f4691886c305e2a87b3e5fc
vsseoane <vsseoane@gmail.com>
2024-01-30 16:02:34 -0300

    Add PaymentCardCostService
    - Add PaymentCardCostService

---
+++ src/main/java/com/etraveli/paymentCardsCostTest/services/imp/PaymentCardCostService.java
@@ -0,0 +1,48 @@
+package com.etraveli.paymentCardsCostTest.services.imp;
+
+import com.etraveli.paymentCardsCostTest.apiCall.IApiCallService;
+import com.etraveli.paymentCardsCostTest.dto.CountryCostMatrixDto;
+import com.etraveli.paymentCardsCostTest.dto.PaymentCardCostDto;
+import com.etraveli.paymentCardsCostTest.dto.PaymentCardCostResponseDto;
+import com.etraveli.paymentCardsCostTest.exceptions.CountryCostMatrixNotFoundException;
+import com.etraveli.paymentCardsCostTest.repository.ICountryCostMatrixRepository;
+import com.etraveli.paymentCardsCostTest.services.ICountryCostMatrixService;
+import com.etraveli.paymentCardsCostTest.services.IPaymentCardCostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PaymentCardCostService implements IPaymentCardCostService {
+
+    private IApiCallService apiCallService;
+    private ICountryCostMatrixService countryCostMatrixService;
+
+    @Autowired
+    public PaymentCardCostService(IApiCallService apiCallService, ICountryCostMatrixService countryCostMatrixService) {
+        this.apiCallService = apiCallService;
+        this.countryCostMatrixService = countryCostMatrixService;
+    }
+    @Override
+    public PaymentCardCostResponseDto calculatePaymentCardCost(PaymentCardCostDto paymentCardCostDto) {
+        try {
+            String country = this.apiCallService.getCountryByIin(paymentCardCostDto.getIin());
+            CountryCostMatrixDto countryCostMatrixDto = this.countryCostMatrixService.getCountryCostMatrixByCountry(country);
+            if (countryCostMatrixDto == null) throw new CountryCostMatrixNotFoundException("Unable to retrieve " +
+                    "cost for the specified IIN: " + paymentCardCostDto.getIin());
+            double costIin = countryCostMatrixDto.getCostUSD();
+            PaymentCardCostResponseDto paymentCardCostResponseDto = new PaymentCardCostResponseDto();
+            paymentCardCostResponseDto.setCountry(country);
+            paymentCardCostResponseDto.setCost(costIin);
+            return paymentCardCostResponseDto;
+        } catch( Exception e ) {
+            System.out.println("Error: " + e.getMessage()); //TODO: Change it for logger
+            throw new CountryCostMatrixNotFoundException("Unable to retrieve " +
+                "cost for the specified IIN");
+
+        }
+
+    }
+
+}
